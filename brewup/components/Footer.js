9ff96eae67136f90acec0edf61d4c4ea9d9167bf
Anarv Vasavada <anarvvasavada@gmail.com>
2025-01-03 20:09:58 +0530

    Initial commit: Added project files

---
+++ brewup/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>BrewUp - Where creators brew the future</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ brewup/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <p>BrewUp - Where creators brew the future</p>
+    </nav>
+  )
+}
+
+export default Navbar
